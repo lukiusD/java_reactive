9b816cb1dd6edeac034437ae357a9207b7f325b2
Lukáš Ďurčák <durcak@24-pay.eu>
2025-01-17 16:20:52 +0100

    Minor changes

--- src/main/java/eu/durcak/java_reactive_web/controller/IndexController.java
+++ src/main/java/eu/durcak/java_reactive_web/controller/IndexController.java
@@ -1,11 +1,14 @@
 package eu.durcak.java_reactive_web.controller;
 
+import ch.qos.logback.core.joran.sanity.Pair;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+import java.text.DecimalFormat;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @RestController
 public class IndexController {
@@ -15,4 +18,10 @@ public class IndexController {
             // Vráti aktuálny čas ako reaktívny Mono
             return Mono.just(LocalDateTime.now().toString());
         }
+
+    @GetMapping(path="/json-time", produces= MediaType.APPLICATION_JSON_VALUE)
+    public Mono<String> getJsonTime() {
+        // Vráti aktuálny čas ako reaktívny Mono
+        return Mono.just(LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE).toString());
+    }
 }
