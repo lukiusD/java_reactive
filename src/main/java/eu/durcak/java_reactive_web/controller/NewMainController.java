9e6663512f249fece567402d3ea00f51b4d2252e
Lukáš Ďurčák <lukas@durcak.eu>
2025-01-18 21:19:35 +0100

    New Controller added NewMainController

--- src/main/java/eu/durcak/java_reactive_web/controller/NewMainController.java
+++ src/main/java/eu/durcak/java_reactive_web/controller/NewMainController.java
@@ -1,10 +1,16 @@
 package eu.durcak.java_reactive_web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.Map;
 
 @Controller
 public class NewMainController {
-    public String exex(){
+
+    @PostMapping("/submit")
+    public String exex(@RequestBody Map<String, Object> postRequestData){
         return "This is the new Main Controller";
     }
 }
