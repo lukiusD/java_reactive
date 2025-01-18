1c8725507b749cf84e462752fc026303e66da83d
Lukáš Ďurčák <lukas@durcak.eu>
2025-01-18 21:16:15 +0100

    New Controller added NewMainController

---
+++ src/main/java/eu/durcak/java_reactive_web/controller/NewMainController.java
@@ -0,0 +1,10 @@
+package eu.durcak.java_reactive_web.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class NewMainController {
+    public String exex(){
+        return "This is the new Main Controller";
+    }
+}
