a06993f8b4382730cbe2874bbfb196a2d4921238
Lukáš Ďurčák <lukas@durcak.eu>
2025-01-18 20:17:17 +0100

    Ready for commit

--- src/main/java/eu/durcak/java_reactive_web/controller/IndexController.java
+++ src/main/java/eu/durcak/java_reactive_web/controller/IndexController.java
@@ -1,27 +1,42 @@
 package eu.durcak.java_reactive_web.controller;
 
-import ch.qos.logback.core.joran.sanity.Pair;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
-import java.text.DecimalFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.format.SignStyle;
+
+import static java.time.temporal.ChronoField.*;
 
 @RestController
 public class IndexController {
 
-        @GetMapping(path="/time", produces= MediaType.APPLICATION_JSON_VALUE)
-        public Mono<String> getTime() {
-            // Vráti aktuálny čas ako reaktívny Mono
-            return Mono.just(LocalDateTime.now().toString());
-        }
+    @GetMapping(path="/time", produces= MediaType.APPLICATION_JSON_VALUE)
+    public Mono<String> getTime() {
+        // Vráti aktuálny čas ako reaktívny Mono
+        return Mono.just(LocalDateTime.now().toString());
+    }
 
     @GetMapping(path="/json-time", produces= MediaType.APPLICATION_JSON_VALUE)
-    public Mono<String> getJsonTime() {
+    public Mono<TimeData> getJsonTime() {
         // Vráti aktuálny čas ako reaktívny Mono
-        return Mono.just(LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE).toString());
+
+        DateTimeFormatter dateFormat = new DateTimeFormatterBuilder()
+                .appendValue(DAY_OF_MONTH, 2)
+                .appendLiteral('.')
+                .appendValue(MONTH_OF_YEAR, 2)
+                .appendLiteral('.')
+                .appendValue(YEAR, 4, 10, SignStyle.EXCEEDS_PAD)
+                .toFormatter();
+
+        return Mono.just(new TimeData(LocalDateTime.now().format(dateFormat).toString()));
     }
+
+    public record TimeData(String time){}
 }
+
+
