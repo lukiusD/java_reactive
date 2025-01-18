57980a2565cb6101ac5de97476c25dc414efa16e
Lukáš Ďurčák <lukas@durcak.eu>
2025-01-18 21:22:12 +0100

    Fun

--- src/main/java/eu/durcak/java_reactive_web/controller/NewMainController.java
+++ src/main/java/eu/durcak/java_reactive_web/controller/NewMainController.java
@@ -3,6 +3,7 @@ package eu.durcak.java_reactive_web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 import java.util.Map;
 
@@ -10,7 +11,8 @@ import java.util.Map;
 public class NewMainController {
 
     @PostMapping("/submit")
-    public String exex(@RequestBody Map<String, Object> postRequestData){
+    public String exex(@RequestBody Map<String, Object> postRequestData,
+                       @RequestHeader(value = "X-author", required = false) String author){
         return "This is the new Main Controller";
     }
 }
