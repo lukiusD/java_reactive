970b222215453625690be66e2f0fcfb9e2047275
Lukáš Ďurčák <lukas@durcak.eu>
2025-01-18 20:32:08 +0100

    Experimental commit

--- src/main/java/eu/durcak/java_reactive_web/controller/ExperimentController.java
+++ src/main/java/eu/durcak/java_reactive_web/controller/ExperimentController.java
@@ -1,7 +1,9 @@
 package eu.durcak.java_reactive_web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class ExperimentController {
 
     @GetMapping("/experimental")
